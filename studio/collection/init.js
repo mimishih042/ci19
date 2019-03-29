fae6fec93011d51b06f29d1b7558b82b6458c25d
Mimi Shih <mimishih@mimis-mbp-3.wireless.newschool.edu>
2019-03-29 10:36:30 -0400

    iteration 2

--- studio/collection/init.js
+++ studio/collection/init.js
@@ -1,15 +1,39 @@
 $(document).ready(function() {
-
+  var x = document.getElementById("thirtyone");   // Get the element with id="demo"
+  randPosition(x);
+  var y = document.getElementById("one");   // Get the element with id="demo"
+  randPosition(y);
 
 });
 
-function showNews(input){
-  var id = document.getElementById(input);
-  document.body.id.style.left = 500;  
+
+function randPosition(element){
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    var left = (Math.floor(Math.random() * w)) + "px" ;
+    var top = (Math.floor(Math.random() * h)) + "px" ;
+    element.style.left = left;
+    element.style.top = top;
+    console.log(left, top);
+    console.log(w,h);
 }
 
-
-function change(num){
-    var colors = ["#ffffff", "#f1f1f1", "#F8AA23", "#ff6737", "#CE220D", "#ff79e5", "#5EB9F7"]; 
-    document.body.style.backgroundColor = colors[num] ;   
+function mouseOver(element){
+    element.style.zIndex = -1 ;
+    element.style.opacity = 0.5;   
 }
+
+function mouseOut(element){
+    element.style.opacity = 1;
+       
+}
+
+function fullImg(imgNum){
+    var elem = document.getElementById("append");
+    var newImg = document.createElement("img");
+    newImg.src = imgNum ;
+    newImg.className = "centered";
+    elem.appendChild(newImg);
+    $(".element").css("opacity", "0.5");
+  }
+
