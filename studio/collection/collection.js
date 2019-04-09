9f31f779f728da48f337e6a16a539a64640dfac9
Mimi Shih <mimishih@Mimis-MacBook-Pro-3.local>
2019-04-08 23:02:21 -0400

    collection json update

--- studio/collection/collection.js
+++ studio/collection/collection.js
@@ -1,1 +1,4 @@
-
+var url = "https://raw.githubusercontent.com/sssssuty/ci19/master/lab/March.27/water.json";
+  fetch(url)
+    .then(function(response){return response.json();})
+    .then(function(json){console.log(json);})
