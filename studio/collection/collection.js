af2b08d6caf82d0cdf7da6d3be5c1ef4a41340d1
Mimi Shih <mimishih@Mimis-MacBook-Pro-3.local>
2019-04-08 23:46:44 -0400

    update

--- studio/collection/collection.js
+++ studio/collection/collection.js
@@ -1,4 +1,32 @@
-var url = "https://raw.githubusercontent.com/sssssuty/ci19/master/lab/March.27/water.json";
+var url = "https://mimishih042.github.io/ci19/studio/collection/data.json";
   fetch(url)
     .then(function(response){return response.json();})
-    .then(function(json){console.log(json);})
+    .then(function(json){buildSection(json);})
+
+function buildSection(data){
+  for (var i = 0; i < data.length; i++) {
+    var box = document.createElement("div");
+    box.className = "box";
+
+    var headline = document.createElement("h1");
+    headline.className = "headlines";
+    headline.innerHTML = data[i].headline;
+    box.appendChild(headline);
+
+    var unclickedImg = document.createElement("img");
+    unclickedImg.className = "edit";
+    unclickedImg.src = data[i].edit;
+    box.appendChild(unclickedImg);
+
+    var yearBox = document.createElement("div");
+    yearBox.className = "boxYear";
+    yearBox.innerHTML = data[i].year ;
+
+    var elemContainer = document.getElementById("elemContainer");
+    elemContainer.appendChild(box);
+    elemContainer.appendChild(yearBox);
+
+    console.log(elemContainer);
+  }
+
+}
